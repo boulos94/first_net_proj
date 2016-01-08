421562fbf7d992ede836e19f3983c810150493d4
boulos <boulos@boulos>
2016-01-08 05:47:45 +0200

    more organisation (+ open pic on click)

--- public_html/javaScript.js
+++ public_html/javaScript.js
@@ -1,2 +1,413 @@
-
-
+ var cImage;
+            function changeImage(){
+                cImage = window.setInterval(getImage1, 2000);
+                cImage = window.setInterval(getImage2, 2000);
+                cImage = window.setInterval(getImage3, 2000);
+                cImage = window.setInterval(getImage4, 2000);
+                cImage = window.setInterval(getImage5, 2000);
+                cImage = window.setInterval(getImage6, 2000);
+                cImage = window.setInterval(getImage7, 2000);
+                cImage = window.setInterval(getImage8, 2000);
+                cImage = window.setInterval(getImage9, 2000);
+                cImage = window.setInterval(getImage10, 2000);
+                cImage = window.setInterval(getImage11, 2000);
+                cImage = window.setInterval(getImage12, 2000);
+                cImage = window.setInterval(getImage13, 2000);
+                cImage = window.setInterval(getImage14, 2000);
+                cImage = window.setInterval(getImage15, 2000);
+                cImage = window.setInterval(getImage16, 2000);
+                cImage = window.setInterval(getImage17, 2000);
+                cImage = window.setInterval(getImage18, 2000);
+                cImage = window.setInterval(getImage19, 2000);
+                cImage = window.setInterval(getImage20, 2000);
+                cImage = window.setInterval(getImage21, 2000);
+                cImage = window.setInterval(getImage22, 2000);
+                cImage = window.setInterval(getImage23, 2000);
+                cImage = window.setInterval(getImage24, 2000);
+            }
+                var image;
+                var rnd = Math.random();
+            function getImage1(){
+                
+                var rnd = Math.random();
+                if(rnd<0.33){
+                    image = "4x41.jpg";
+                    document.getElementById("s41").href="4x41.jpg";
+                }else if(rnd<0.66){
+                    image = "41.jpg";
+                    document.getElementById("s41").href="41.jpg";
+                }else if (rnd<0.99) {
+                    image = "411.png";
+                    document.getElementById("s41").href="411.png";
+                }
+                document.getElementById("41").src 
+                        = image;
+            }
+            function getImage2(){
+                
+                if(rnd<0.33){
+                    image = "4x42.jpg";
+                    document.getElementById("s42").href="4x42.jpg";
+                }else if(rnd<0.66){
+                    image = "42.jpg";
+                    document.getElementById("s42").href="42.jpg";
+                }else if (rnd<0.99) {
+                    image = "422.jpg";
+                    document.getElementById("s42").href="422.jpg";
+                }
+                document.getElementById("42").src 
+                        = image;
+            }
+            function getImage3(){
+                
+                if(rnd<0.33){
+                    image = "4x43.jpg";
+                    document.getElementById("s43").href="4x43.jpg";
+                }else if(rnd<0.66){
+                    image = "43.jpg";
+                    document.getElementById("s43").href="43.jpg";
+                }else if (rnd<0.99) {
+                    image = "433.jpg";
+                    document.getElementById("s43").href="433.jpg";
+                }
+                document.getElementById("43").src 
+                        = image;
+            }
+            function getImage4(){
+               
+                if(rnd<0.33){
+                    image = "4x44.jpg";
+                    document.getElementById("s44").href="4x44.jpg";
+                }else if(rnd<0.66){
+                    image = "44.jpg";
+                    document.getElementById("s44").href="44.jpg";
+                }else if (rnd<0.99) {
+                    image = "444.jpg";
+                    document.getElementById("s44").href="444.jpg";
+                }
+                document.getElementById("44").src 
+                        = image;
+            }
+            function getImage5(){
+                
+                if(rnd<0.33){
+                    image = "4x45.jpg";
+                    document.getElementById("s45").href="4x45.jpg";
+                }else if(rnd<0.66){
+                    image = "45.jpg";
+                    document.getElementById("s45").href="45.jpg";
+                }else if (rnd<0.99) {
+                    image = "455.jpg";
+                    document.getElementById("s45").href="455.jpg";
+                }
+                document.getElementById("45").src 
+                        = image;
+            }
+            function getImage6(){
+                
+                if(rnd<0.33){
+                    image = "4x46.jpg";
+                    document.getElementById("s46").href="4x46.jpg";
+                }else if(rnd<0.66){
+                    image = "46.jpg";
+                    document.getElementById("s46").href="46.jpg";
+                }else if (rnd<0.99) {
+                    image = "466.jpg";
+                    document.getElementById("s46").href="466.jpg";
+                }
+                document.getElementById("46").src 
+                        = image;
+            }
+            
+            function getImage7(){
+                               
+                if(rnd<0.33){
+                    image = "car1.jpg";
+                    document.getElementById("sc1").href="car1.jpg";
+                }else if(rnd<0.66){
+                    image = "c1.jpg";
+                    document.getElementById("sc1").href="c1.jpg";
+                }else if (rnd<0.99) {
+                    image = "c11.jpg";
+                    document.getElementById("sc1").href="c11.jpg";
+                }
+                document.getElementById("c1").src 
+                        = image;
+            }
+            function getImage8(){
+                
+                if(rnd<0.33){
+                    image = "car2.jpg";
+                    document.getElementById("sc2").href="car2.jpg";
+                }else if(rnd<0.66){
+                    image = "c2.jpg";
+                    document.getElementById("sc2").href="c2.jpg";
+                }else if (rnd<0.99) {
+                    image = "c22.jpg";
+                    document.getElementById("sc2").href="c22.jpg";
+                }
+                document.getElementById("c2").src 
+                        = image;
+            }
+            function getImage9(){
+               
+                var rnd = Math.random();
+                if(rnd<0.33){
+                    image = "car3.jpg";
+                    document.getElementById("sc3").href="car3.jpg";
+                }else if(rnd<0.66){
+                    image = "c3.jpg";
+                    document.getElementById("sc3").href="c3.jpg";
+                }else if (rnd<0.99) {
+                    image = "c33.jpg";
+                    document.getElementById("sc3").href="c33.jpg";
+                }
+                document.getElementById("c3").src 
+                        = image;
+            }
+            function getImage10(){
+                
+                if(rnd<0.33){
+                    image = "car4.jpg";
+                    document.getElementById("sc4").href="car4.jpg";
+                }else if(rnd<0.66){
+                    image = "c4.jpg";
+                    document.getElementById("sc4").href="c4.jpg";
+                }else if (rnd<0.99) {
+                    image = "c44.jpg";
+                    document.getElementById("sc4").href="c44.jpg";
+                }
+                document.getElementById("c4").src 
+                        = image;
+            }
+            function getImage11(){
+               
+                if(rnd<0.33){
+                    image = "car5.jpg";
+                    document.getElementById("sc5").href="car5.jpg";
+                }else if(rnd<0.66){
+                    image = "c5.jpg";
+                    document.getElementById("sc5").href="c5.jpg";
+                }else if (rnd<0.99) {
+                    image = "c55.jpg";
+                    document.getElementById("sc5").href="c55.jpg";
+                }
+                document.getElementById("c5").src 
+                        = image;
+            }
+            function getImage12(){
+                
+                if(rnd<0.33){
+                    image = "car6.jpg";
+                    document.getElementById("sc").href="car6.jpg";
+                }else if(rnd<0.66){
+                    image = "c6.jpg";
+                    document.getElementById("sc6").href="c6.jpg";
+                }else if (rnd<0.99) {
+                    image = "c66.jpg";
+                    document.getElementById("sc6").href="c66.jpg";
+                }
+                document.getElementById("c6").src 
+                        = image;
+            }
+           
+            
+            function getImage13(){
+               
+                if(rnd<0.33){
+                    image = "moto1.jpg";
+                    document.getElementById("sm1").href="moto1.jpg";
+                }else if(rnd<0.66){
+                    image = "m1.jpg";
+                    document.getElementById("sb6").href="m1.jpg";
+                }else if (rnd<0.99) {
+                    image = "m11.jpg";
+                    document.getElementById("sb6").href="m11.jpg";
+                }
+                document.getElementById("m1").src 
+                        = image;
+            }
+            function getImage14(){
+                
+                if(rnd<0.33){
+                    image = "moto2.jpg";
+                    document.getElementById("sm2").href="moto2.jpg";
+                }else if(rnd<0.66){
+                    image = "m2.jpg";
+                    document.getElementById("sm2").href="m2.jpg";
+                }else if (rnd<0.99) {
+                    image = "m22.jpg";
+                    document.getElementById("sm2").href="m22.jpg";
+                }
+                document.getElementById("m2").src 
+                        = image;
+            }
+            function getImage15(){
+                
+                if(rnd<0.33){
+                    image = "moto3.jpg";
+                    document.getElementById("sm3").href="moto3.jpg";
+                }else if(rnd<0.66){
+                    image = "m3.jpg";
+                    document.getElementById("sm3").href="m3.jpg";
+                }else if (rnd<0.99) {
+                    image = "m33.jpg";
+                    document.getElementById("sm3").href="m33.jpg";
+                }
+                document.getElementById("m3").src 
+                        = image;
+            }
+            function getImage16(){
+                
+                if(rnd<0.33){
+                    image = "moto4.jpg";
+                    document.getElementById("sm4").href="moto4.jpg";
+                }else if(rnd<0.66){
+                    image = "m4.jpg";
+                    document.getElementById("sm4").href="m4.jpg";
+                }else if (rnd<0.99) {
+                    image = "m44.jpg";
+                    document.getElementById("sm4").href="m44.jpg";
+                }
+                document.getElementById("m4").src 
+                        = image;
+            }
+            function getImage17(){
+                
+                if(rnd<0.33){
+                    image = "moto5.jpg";
+                    document.getElementById("sm5").href="moto5.jpg";
+                }else if(rnd<0.66){
+                    image = "m5.jpg";
+                    document.getElementById("sm5").href="m5.jpg";
+                }else if (rnd<0.99) {
+                    image = "m55.jpg";
+                    document.getElementById("sm5").href="m55.jpg";
+                }
+                document.getElementById("m5").src 
+                        = image;
+            }
+            function getImage18(){
+                
+                if(rnd<0.33){
+                    image = "moto6.jpg";
+                    document.getElementById("sm6").href="moto6.jpg";
+                }else if(rnd<0.66){
+                    image = "m6.jpg";
+                    document.getElementById("sm6").href="m6.jpg";
+                }else if (rnd<0.99) {
+                    image = "m66.jpg";
+                    document.getElementById("sm6").href="m66.jpg";
+                }
+                document.getElementById("m6").src 
+                        = image;
+            }
+            
+            function getImage19(){
+                
+                if(rnd<0.33){
+                    image = "boat1.jpg";
+                    document.getElementById("sb1").href="boat1.jpg";
+                }else if(rnd<0.66){
+                    image = "b11.jpg";
+                    document.getElementById("sb1").href="b11.jpg";
+                }else if (rnd<0.99) {
+                    image = "b111.jpg";
+                    document.getElementById("sb1").href="b11.jpg";
+                }
+                document.getElementById("b1").src 
+                        = image;
+            }
+            function getImage20(){
+                
+                if(rnd<0.33){
+                    image = "boat2.jpg";
+                    document.getElementById("sb2").href="boat2.jpg";
+                }else if(rnd<0.66){
+                    image = "b22.jpg";
+                    document.getElementById("sb2").href="b22.jpg";
+                }else if (rnd<0.99) {
+                    image = "b222.jpg";
+                    document.getElementById("sb2").href="b222.jpg";
+                }
+                document.getElementById("b2").src 
+                        = image;
+            }
+            function getImage21(){
+                
+                if(rnd<0.33){
+                    image = "boat3.jpg";
+                    document.getElementById("sb3").href="boat3.jpg";
+                }else if(rnd<0.66){
+                    image = "b33.jpg";
+                    document.getElementById("sb3").href="b33.jpg";
+                }else if (rnd<0.99) {
+                    image = "b333.jpg";
+                    document.getElementById("sb3").href="b333.jpg";
+                }
+                document.getElementById("b3").src 
+                        = image;
+            }
+            function getImage22(){
+                
+                if(rnd<0.33){
+                    image = "boat4.jpg";
+                    document.getElementById("sb4").href="boat4.jpg";
+                }else if(rnd<0.66){
+                    image = "b44.jpg";
+                    document.getElementById("sb4").href="b44.jpg";
+                }else if (rnd<0.99) {
+                    image = "b444.jpg";
+                    document.getElementById("sb4").href="b444.jpg";
+                }
+                document.getElementById("b4").src 
+                        = image;
+            }
+            function getImage23(){
+                
+                if(rnd<0.33){
+                    image = "boat5.jpg";
+                    document.getElementById("sb5").href="boat5.jpg";
+                }else if(rnd<0.66){
+                    image = "b55.jpg";
+                    document.getElementById("sb5").href="b55.jpg";
+                }else if (rnd<0.99) {
+                    image = "b555.jpg";
+                    document.getElementById("sb5").href="b555.jpg";
+                }
+                document.getElementById("b5").src 
+                        = image;
+            }
+            function getImage24(){
+                
+                if(rnd<0.33){
+                    image = "boat6.jpg";
+                    document.getElementById("sb6").href="boat6.jpg";
+                }else if(rnd<0.66){
+                    image = "b66.jpg";
+                    document.getElementById("sb6").href="b66.jpg";
+                }else if (rnd<0.99) {
+                    image = "b666.jpg";
+                    document.getElementById("sb6").href="b666.jpg";
+                }
+                document.getElementById("b6").src 
+                        = image;
+            
+            }
+            var coloring;
+            function changeColor(){
+                coloring = window.setInterval(getColor, 2000);
+            }
+            function getColor(){
+                var color;
+                var rnd = Math.random();
+                if(rnd<0.34){
+                    color = "white";
+                }else if(rnd<0.67){
+                    color = "black";
+                }else {
+                    color = "grey";
+                }
+                document.getElementById("reg").style.color 
+                        = color;
+            }
